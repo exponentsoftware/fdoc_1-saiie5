c0b8560f6cfd6d4c895fbc64db07b9b8b94eee9f
saiie5 <56484744+saiie5@users.noreply.github.com>
2021-08-26 10:52:20 +0530

    unique array

--- problem2.js
+++ problem2.js
@@ -40,6 +40,25 @@ return arr;
   console.log(reverseArray(['A','B','C']));
 
 
+///2.d. Write a function which check if items of an array are unique?
+
+function checkUniqueness(arr)
+{
+    
+    let s = new Set();
+    for (let i = 0; i < arr.length; i++) {
+        s.add(arr[i]);
+    }
+ 
+    
+    return (s.size == arr.length);
+    
+}
+const arrOne = [1, 4, 6, 2, 1];
+console.log(checkUniqueness(arrOne));
+
+const arrTwo = [1, 4, 6, 2, 3]
+console.log(checkUniqueness(arrTwo));
 
 
  
