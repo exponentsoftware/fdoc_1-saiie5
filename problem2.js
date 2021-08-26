f2ede5ad9fe0fbba65522758dcb78ed9b5ac4a05
saiie5 <56484744+saiie5@users.noreply.github.com>
2021-08-26 10:51:03 +0530

    returns seven random numbers

--- problem2.js
+++ problem2.js
@@ -14,3 +14,19 @@ function loopingTriangle() {
 
 
 
+ //2.b. Write a function which returns array of seven random numbers in a range of 0-9. All the numbers must be unique
+
+ function sevenRandomNumbers() {
+    var arr = [];
+while(arr.length < 8){
+    var r = Math.floor(Math.random() * 10) ;
+    if(arr.indexOf(r) === -1) arr.push(r);
+}
+return arr;
+}
+ console.log(sevenRandomNumbers())
+
+
+ //2c. Reversing an array: Arrays have a reverse method which changes the array by inverting the order in which its elements appear. For this exercise, write a function, reverseArray. The reverseArray, takes an array as argument and produces a new array that has the same elements in the inverse order. Without reverse method.
+
+
